7d8a4ee529ff83ed9260e3b2fd4340f36e0dc466
wanggege <1914161290@qq.com>
2019-08-23 10:07:15 +0800

    配置路径

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,8 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  API_ROOT: '"/Travel-self/dist/"'
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/Travel-self/dist/'
+  //   : '/',
 }
